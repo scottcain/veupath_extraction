bebf170d36c7382a0445b98d15c2a62e31948037
Scott Cain <scott+git@scottcain.net>
2024-11-27 10:43:54 -0800

    multiple updates: generalizing GET string creation and parameterizing assembly

--- extractor.pl
+++ extractor.pl
@@ -8,10 +8,10 @@ use JSON;
 use Bio::GFF3::LowLevel qw / gff3_format_feature  /;
 use URI::Escape;
 
-my $ASSEMBLY = 'pfal3D7';
+my $ASSEMBLY = $ARGV[0];
 my $SEQINFO              = "https://plasmodb.org/a/service/jbrowse/seq/$ASSEMBLY";
 my $TRACKINFO            = "https://plasmodb.org/a/jbrowse/tracks/$ASSEMBLY/tracks.conf";
-my $ASSEMBLYSPECIFICCONT = "https://plasmodb.org/a/service/jbrowse/organismSpecific/$ASSEMBLY" #json file
+my $ASSEMBLYSPECIFICCONT = "https://plasmodb.org/a/service/jbrowse/organismSpecific/$ASSEMBLY"; #json file
 my $RNASEQJUNCTIONS      = "https://plasmodb.org/a/service/jbrowse/rnaseqJunctions/$ASSEMBLY"; #json file
 
 #this is the standard tracks.conf; ignores for the moment other track configs
@@ -23,7 +23,7 @@ my $RNASEQJUNCTIONS      = "https://plasmodb.org/a/service/jbrowse/rnaseqJunctio
 #fetch tracks.conf
 ###
 my $TRACK_CONF = "tracks.conf";
-system("curl -o $TRACK_CONF $TRACKINFO) == 0 or die;
+system("curl -o $TRACK_CONF $TRACKINFO") == 0 or die;
 my %vuepath_track_info;
 my %lh;
 my $track_key;
@@ -46,7 +46,67 @@ close TRACKCONF;
 ###
 #fetch other json config files
 ###
+my $JUNC_FILE = $ASSEMBLY."_junc.json";
+system("curl -o $JUNC_FILE $RNASEQJUNCTIONS") == 0 or die;
+my $junc_blob;
+{
+    local $/ = undef;
+    my $file = $JUNC_FILE;
+    open FF, "<$file" or die "couldn't open $file: $!";
+    $junc_blob = <FF>;
+    close FF;
+}
+my $junc_tracks = JSON->new->decode($junc_blob);
 
+my $SPEC_SPEC = $ASSEMBLY."_species_specific.json";
+system("curl -o $SPEC_SPEC $ASSEMBLYSPECIFICCONT") == 0 or die;
+my $ss_blob;
+{
+    local $/ = undef;
+    my $file = $SPEC_SPEC;
+    open FF, "<$file" or die "couldn't open $file: $!";
+    $ss_blob = <FF>;
+    close FF;
+}
+my $ss_tracks = JSON->new->decode($ss_blob);
+
+###
+#   Map the jbrowse json configs to the internal config hash
+###
+#print Dumper($$ss_tracks{'tracks'});
+#print @{$$junc_tracks{'tracks'}};
+#print "\n----------\n";
+#print @{$$ss_tracks{'tracks'}};
+push @{$$ss_tracks{'tracks'}}, @{$$junc_tracks{'tracks'}};
+for my $hashref (@{$$ss_tracks{'tracks'}}) {
+    next unless defined $$hashref{'key'};
+    my $track_key = $$hashref{'key'};
+    next unless $$hashref{'storeClass'} =~ /SeqFeature\/REST/i;
+    next if defined $$hashref{'subtracks'};
+
+    for my $key (keys %{$hashref}) {
+	    
+            next if $key eq 'key';
+	    if ($key eq 'query') {
+                for my $qkey (keys %{$$hashref{'query'}}) {
+                    $vuepath_track_info{$track_key}{"query.$qkey"} = $$hashref{'query'}{$qkey};
+		    #print "$track_key\t$qkey\n";
+	        }
+                next;
+	    }
+            if ($key eq 'metadata') {
+                for my $mkey (keys %{$$hashref{'metadata'}}) {
+                    $vuepath_track_info{$track_key}{"metadata.$mkey"} = $$hashref{'metadata'}{$mkey};
+                }
+  	        next;
+	    }
+
+   	    $vuepath_track_info{$track_key}{$key} = $$hashref{$key};
+    }
+}
+
+#print JSON->new->pretty->encode(\%vuepath_track_info);
+#die;
 ###
 #fetch contig/refseqs file
 ###
@@ -63,33 +123,49 @@ my $contiglist_blob;
 }
 my $contig_json = JSON->new->decode($contiglist_blob);
 
-
 ###
 # loop through all of the tracks
 ###
 
+my $log = $ASSEMBLY."_tracks.log";
+open LOG, ">$log" or die;
 for my $tr_key (keys %vuepath_track_info) {
+    next unless (defined $vuepath_track_info{$tr_key}{'query.panId'});
     next if (defined $vuepath_track_info{$tr_key}{'query.feature'} &&
             $vuepath_track_info{$tr_key}{'query.feature'} eq 'ReferenceSequence');
-    next unless defined $vuepath_track_info{$tr_key}{'query.edName'};
+    #   next unless defined $vuepath_track_info{$tr_key}{'query.edName'};
 
     if (defined $vuepath_track_info{$tr_key}{'query.edname'}) {
         $vuepath_track_info{$tr_key}{'query.edName'} = $vuepath_track_info{$tr_key}{'query.edname'};
     }
 
-    my $track_name    = uri_escape($vuepath_track_info{$tr_key}{'key'});
-    my $query_feature = uri_escape($vuepath_track_info{$tr_key}{'query.feature'});
-    my $edname_str    = '';
-    if (defined $vuepath_track_info{$tr_key}{'query.edName'}) {
-        $edname_str = '&edName='.uri_escape($vuepath_track_info{$tr_key}{'query.edName'});
-    } else {
-        die Dumper(%vuepath_track_info);
+    my @GETarray;
+    for my $config_key (keys %{$vuepath_track_info{$tr_key}}) {
+        if ( $config_key =~ /query\.(.*)/ ) {
+            my $subkey = $1;
+            push @GETarray, "$subkey=".uri_escape($vuepath_track_info{$tr_key}{$config_key});
+        }
     }
-    next unless $query_feature;
+    my $GETstr     = join("&", @GETarray) if @GETarray >0 ;
+    my $GETnamestr = join("_", @GETarray) if @GETarray >0 ;
+    $GETnamestr   =~ s/\=/_/g;
+
+    my $track_name    = uri_escape($vuepath_track_info{$tr_key}{'key'});
+    $track_name = uri_escape($vuepath_track_info{$tr_key}{'label'}) unless $track_name;
+    $track_name =~ s/\ /_/g;
+    #my $query_feature = uri_escape($vuepath_track_info{$tr_key}{'query.feature'});
+    #my $edname_str    = '';
+    #if (defined $vuepath_track_info{$tr_key}{'query.edName'}) {
+    #    $edname_str = '&edName='.uri_escape($vuepath_track_info{$tr_key}{'query.edName'});
+    #} else {
+    #    die Dumper($vuepath_track_info{$tr_key});
+    #}
+    #next unless $query_feature;
     #    warn $edname_str;
 
-    my  $OUT = $edname_str ? $ASSEMBLY.'_'.uri_escape($vuepath_track_info{$tr_key}{'query.edName'}).'.gff' 
-                           : $ASSEMBLY.'_'.$track_name.'.gff';
+    my  $OUT = $ASSEMBLY.'_'.$track_name;
+        $OUT .= "_$GETnamestr" if $GETnamestr;
+	$OUT .= ".gff";
     open OUT, ">$OUT" or die "couldn't open $OUT for writing: $!"; 
 
     print OUT "##gff-version 3\n";
@@ -102,11 +178,9 @@ for my $tr_key (keys %vuepath_track_info) {
         my $contig_name = $$contig_info{'name'};
 	my $contig_end  = $$contig_info{'end'};
   
-	my $json_file = $contig_name.'_'.$query_feature;
-	$json_file    = $json_file .'_'. uri_escape($vuepath_track_info{$tr_key}{'query.edName'}) 
-	                  if defined $vuepath_track_info{$tr_key}{'query.edName'};
-	$json_file    = "$json_file.json";
-        my $fetch_url = "https://plasmodb.org/a/service/jbrowse/features/$contig_name?feature=$query_feature&start=0&end=$contig_end$edname_str";
+	my $json_file = $contig_name.'_'.$OUT.'.json';
+        my $fetch_url = "https://plasmodb.org/a/service/jbrowse/features/$contig_name?start=0&end=$contig_end";
+	   $fetch_url .= '&'.$GETstr if $GETstr;
 	my $curl = "curl -o $json_file \"$fetch_url\"";
         warn $curl;
 
@@ -127,10 +201,11 @@ for my $tr_key (keys %vuepath_track_info) {
         for my $feature (@{$$json{'features'}}) {
             &parse_line(undef, $contig_name, $feature);
         }
-	sleep 3; # 3 seconds between curls
+	sleep 10; # 3 seconds between curls
     }
     close OUT;
-    sleep 10; # 10 seconds between tracks
+    print LOG "$OUT\n";
+    sleep 60; # 10 seconds between tracks
     die;
 }
 exit 0;
@@ -152,10 +227,13 @@ sub parse_line {
     $gff3_feature{'start'}  = $$f{'startm'}; # the base coord start
     $gff3_feature{'end'}    = $$f{'end'};
     $gff3_feature{'phase'}  = $$f{'phase'};
-    if ($$f{'strand'} == 1) {
+    if (!defined $$f{'strand'}) {
+        $gff3_feature{'strand'} = '.';
+    }
+    elsif ($$f{'strand'} eq '1') {
         $gff3_feature{'strand'} = '+';
     }
-    elsif ($$f{'strand'} == -1) {
+    elsif ($$f{'strand'} eq '-1') {
         $gff3_feature{'strand'} = '-';
     }
     else {
